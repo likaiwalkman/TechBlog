e91c6fecb7f45204a1d88dddb65655d706dc1497
Victor Lee <likaiwalkman@qq.com>
2021-09-08 13:57:25 +0800

    Create backsapce.sh

---
+++ shell/backsapce.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+while true
+do
+  now=`date`;
+  #current_timestamp: Wed Sep  8 13:47:58 CST 2021
+  echo -ne "current_timestamp: $now\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+  #echo -e "OK! \n";
+  sleep 1;
+done
